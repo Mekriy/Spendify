29aac7d57dab19f6abd38931f63c7306839fa2b0
Andrii Pasternak <a.pasternak@nltu.lviv.ua>
2024-03-19 13:45:40 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderComponent
+  },
+  {
+    path:'main-page',
+    component: MainPageComponent
+  },
+  {
+    path:'form',
+    component: FormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
