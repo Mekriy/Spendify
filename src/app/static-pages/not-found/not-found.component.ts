d99d2386429a1cb2cabf3e437fc70692e43e75d5
Taras Yaremkiv <tiaremkiv@exoft.net>
2024-05-02 09:39:31 +0300

    review

--- src/app/static-pages/not-found/not-found.component.ts
+++ src/app/static-pages/not-found/not-found.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
-  providers: [Router]
+  providers: [Router],
 })
 export class NotFoundComponent {
   constructor(private readonly router: Router) {}
 
   ngOnInit() {
+    //TODO: why do you redirect user after 5000? are you sure it's enough to see the problem?
     setTimeout(() => {
       this.router.navigateByUrl('/').then(() => window.location.reload());
     }, 5000);
